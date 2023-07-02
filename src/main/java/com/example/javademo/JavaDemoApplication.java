ee50d52d3bd8bf52211c2688a816c0c2cf7a90dd
LOGOS125-GALAXY\qkrtj <qkrtjdrbs125@naver.com>
2023-07-02 12:13:40 +0900

    comment added

--- src/main/java/com/example/javademo/JavaDemoApplication.java
+++ src/main/java/com/example/javademo/JavaDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaDemoApplication {
 
 	public static void main(String[] args) {
-		//comment added here
+		//comment added here..
 		SpringApplication.run(JavaDemoApplication.class, args);
 	}
 
